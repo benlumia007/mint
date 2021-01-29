bde8c7f363322ed8f91bd72c94f0b8a622da5eda
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-01-28 22:57:20 -0800

    initial commit

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Mint ( functions.php )
+ *
+ * @package   Mint
+ * @copyright Copyright (C) 2021. Benjamin Lu
+ * @license   GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author    Benjamin Lu ( https://getbenonit.com )
+ */
+
+/**
+ * Define namespace
+ */
+namespace Mint;
+
+/**
+ * 2.0 - Backdrop Core
+ */
+if ( file_exists( get_parent_theme_file_path( '/vendor/autoload.php' ) ) ) {
+	require_once get_parent_theme_file_path( '/vendor/autoload.php' );
+}
