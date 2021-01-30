1b3fad10fbc782d12e8f8e26bb70960bd411c34c
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-01-29 17:20:47 -0800

    add add_theme_support( 'editor-styles' );

--- app/functions-setup.php
+++ app/functions-setup.php
@@ -42,7 +42,10 @@ add_action( 'after_setup_theme', function() {
 		/**
 		 * By add_theme_support( 'wp-block-styles' );. This should enable block styles on the frontend.
 		 */
-        add_theme_support( 'wp-block-styles' );
+		add_theme_support( 'wp-block-styles' );
+		
+		add_theme_support( 'editor-styles' );
+		add_editor_style( 'assets/css/editor.css' );
         
 		/**
 		 * By adding add_theme_support( 'align-wide' );. This will enable alignwide and alignfull.
